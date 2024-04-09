267c91ddc29c6af5e38062fa78e495c0981bad95
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2024-04-09 16:08:35 -0400

    feat (MultiThreading): implement Task2

--- Multithreading/MultiThreading.Task2.Chaining/Program.cs
+++ Multithreading/MultiThreading.Task2.Chaining/Program.cs
@@ -6,11 +6,16 @@
  * Fourth Task – calculates the average value. All this tasks should print the values to console.
  */
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task2.Chaining
 {
     class Program
     {
+        const int ArrayLength = 10;
+        const int Min = 0;
+        const int Max = 100;
         static void Main(string[] args)
         {
             Console.WriteLine(".Net Mentoring Program. MultiThreading V1 ");
@@ -21,9 +26,66 @@ namespace MultiThreading.Task2.Chaining
             Console.WriteLine("Fourth Task – calculates the average value. All this tasks should print the values to console");
             Console.WriteLine();
 
-            // feel free to add your code
+            Task<int[]> arrayCreator = Task.Factory.StartNew(() => { 
+                var numbers = new int[ArrayLength];
+                var random = new Random();
+
+                for (int i = 0; i < ArrayLength; i++)
+                {
+                    numbers[i] = random.Next(Min, Max);
+                }
+
+                Console.Write("Generated numbers -> ");
+                Print(numbers);
+                return numbers;
+            });
+
+            Task<int[]> arrayMultiplier = arrayCreator.ContinueWith(task =>
+            {
+                var numbers = new int[ArrayLength];
+                Array.Copy(task.Result, numbers, ArrayLength);
+                var randomNumber = new Random().Next(Min, Max);
+
+                for (int i = 0; i < ArrayLength; i++)
+                {
+                    numbers[i] *= randomNumber;
+                }
+
+                Console.Write(string.Format("Multiplied by {0} -> ", randomNumber));
+                Print(numbers);
+                return numbers;
+            });
+
+            Task<int[]> arraySorter = arrayMultiplier.ContinueWith(task =>
+            {
+                var numbers = new int[ArrayLength];
+                Array.Copy(task.Result, numbers, ArrayLength);
+                Array.Sort(numbers);
+
+                Console.Write("Sorted numbers -> ");
+                Print(numbers);
+                return numbers;
+            });
+
+            Task<double> averageCalculator = arraySorter.ContinueWith(task =>
+            {
+                var average = task.Result.Average();
+
+                Console.WriteLine(string.Format("Average: {0}", average));
+                return average;
+            });
 
             Console.ReadLine();
         }
+
+        static void Print(int[] numbers)
+        {
+            foreach (int num in numbers)
+            {
+                Console.Write(string.Format("{0}  ", num));
+            }
+            Console.WriteLine();
+        }
+
     }
 }
