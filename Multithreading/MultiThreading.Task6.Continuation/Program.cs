19a37814a9d6416ee4e6509dcb2d82196923df00
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2024-04-13 15:50:45 -0400

    feat (MultiThreading): implement Task6

--- Multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ Multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -7,12 +7,14 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace MultiThreading.Task6.Continuation
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Create a Task and attach continuations to it according to the following criteria:");
             Console.WriteLine("a.    Continuation task should be executed regardless of the result of the parent task.");
@@ -22,7 +24,60 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine("Demonstrate the work of the each case with console utility.");
             Console.WriteLine();
 
-            // feel free to add your code
+            var failedParentTask = Task.Run(() =>
+                {
+                    Task.Delay(2000).Wait();
+                    throw new Exception("Parent task failed.");
+                });
+
+            var regardlessResult = failedParentTask.ContinueWith(_ => Console.WriteLine("Continuation task executed regardless of parent task result."));
+            var withoutSuccess = failedParentTask.ContinueWith(_ => Console.WriteLine("Continuation task executed when the parent task finishes without success."), TaskContinuationOptions.NotOnRanToCompletion);
+            var failedAndReused = failedParentTask.ContinueWith(_ => Console.WriteLine("Continuation task executed when the parent task fails (reusing parent task thread)."), TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
+
+            try
+            {
+                Task.WaitAll(failedParentTask, regardlessResult, withoutSuccess, failedAndReused);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            var parentCancellationTokenSource = new CancellationTokenSource();
+            var parentCancellationToken = parentCancellationTokenSource.Token;
+
+            Task cancelledParentTask = Task.Run(() =>
+            {
+                Task.Delay(2000).Wait();
+                // simulate long running operation
+                while (true)
+                {
+                    if (parentCancellationToken.IsCancellationRequested)
+                    {
+                        break;
+                    }
+                }
+            }, parentCancellationToken);
+
+
+            var continuationCancellationTokenSource = new CancellationTokenSource();
+            var continuationCancellationToken = continuationCancellationTokenSource.Token;
+
+            var cancelledAndOutside = cancelledParentTask.ContinueWith(_ =>
+            {
+                Console.WriteLine("Continuation task executed outside of the thread pool when parent task is canceled.");
+            }, continuationCancellationToken, TaskContinuationOptions.OnlyOnCanceled, TaskScheduler.Default);
+
+            parentCancellationTokenSource.Cancel();
+
+            try
+            {
+                Task.WaitAll(cancelledParentTask, cancelledAndOutside);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             Console.ReadLine();
         }
