cf7776de9fdc7e979c51315bf9257acaef835c69
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2024-04-17 22:57:47 -0400

    feat (AsyncAwait): implement Task2

--- AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
+++ AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
@@ -23,6 +23,13 @@ public class StatisticMiddleware
     {
         string path = context.Request.Path;
 
+        /*
+         * 1. Task.Run() should be used for CPU bound tasks and not I/O bound. In StatisticMiddleware case, all tasks are I/O bound (requests to the backend server). Instead, I can await on Tasks returned from _statisticService.
+         * 2. If I remove Thread.Sleep(3000), the exception "System.ObjectDisposedException: 'IFeatureCollection has been disposed." is thrown. I think it happens because continuation created using OnComplete method starts after HttpContext is disposed.
+⁠         * 3. ConfigureAwait(false) should not be here. It tells that it does not need the context, so the code can be run in any thread. But continuation can also be run without context and it uses HttpContext. It's not common, but it can happen and lead to unexpected errors.
+         */
+
+        /*
         var staticRegTask = Task.Run(
             () => _statisticService.RegisterVisitAsync(path)
                 .ConfigureAwait(false)
@@ -37,6 +44,11 @@ public class StatisticMiddleware
         }
 
         Thread.Sleep(3000); // without this the statistic counter does not work
+        */
+
+        await _statisticService.RegisterVisitAsync(path);
+        var visitsCount = await _statisticService.GetVisitsCountAsync(path);
+        context.Response.Headers.Add(CustomHttpHeaders.TotalPageVisits, visitsCount.ToString());
         await _next(context);
     }
 }
